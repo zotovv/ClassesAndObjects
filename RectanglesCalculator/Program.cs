15da32867f7c105e6eb17958fe8153f65ebd0238
Vladimir Zotov <zott14@outlook.com>
2018-05-08 10:43:20 +0300

    Add RectanglesCalculator

---
+++ RectanglesCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectanglesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину 1 стороны прямоугольника: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длину 2 стороны прямоугольника: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine("Area = {0}, Perimeter = {1}", rectangle.Area, rectangle.Perimeter);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RectanglesCalculator/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectanglesCalculator
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+         double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        public double Area
+        {
+            get { return this.AreaCalculator(); }
+        }
+
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        public double Perimeter
+        {
+            get { return this.PerimeterCalculator(); }
+        }
+
+        
+
+        
+            
+    }
+}
