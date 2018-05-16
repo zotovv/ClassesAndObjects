e20b78f793fb96d231d2c5a91610a03746f9cd53
Vladimir Zotov <zott14@outlook.com>
2018-05-16 21:51:11 +0300

    Add Exchange

---
+++ Exchange/Converter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange
+{
+    class Converter
+    {
+        double usd, eur, uah;
+
+        public Converter(double usd, double eur, double uah)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.uah = uah;
+        }
+
+        public void ToUsd(double rubSum)
+        {
+            Console.WriteLine(rubSum / usd);
+        }
+
+        public void FromUsd(double usdSum)
+        {
+            Console.WriteLine(usdSum * usd);
+        }
+
+        public void ToEur(double rubSum)
+        {
+            Console.WriteLine(rubSum / eur);
+        }
+
+        public void FromEur(double eurSum)
+        {
+            Console.WriteLine(eurSum * eur);
+        }
+
+        public void ToUah(double rubSum)
+        {
+            Console.WriteLine(rubSum / uah);
+        }
+
+        public void FromUah(double uahSum)
+        {
+            Console.WriteLine(uahSum*uah);
+        }
+    }
+}
---
+++ Exchange/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(62.3, 73.8, 2.3);
+
+            converter.FromUsd(120);
+            converter.ToUsd(25000);
+
+            Console.ReadKey();
+        }
+    }
+}
