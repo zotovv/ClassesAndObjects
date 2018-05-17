cc0459ed21d16cb7a87d1738661a5b076dc124a4
Vladimir Zotov <zott14@outlook.com>
2018-05-17 11:00:06 +0300

    Add  Salary

---
+++ Salery/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Employee
+    {
+        readonly string name, surname;
+        int experience;
+        string post;
+
+        public string Surname
+        {
+            get => surname;
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public string Post
+        {
+            get
+            {
+                if (post == null)
+                    return "Должность неизвестна.";
+                return post;
+            }
+            set
+            {
+                if (value != null)
+                    post = value;
+            }
+        }
+
+        public int Experience
+        {
+            get => experience;
+            set
+            {
+                if (value >= 0)
+                    experience = value;
+            }
+        }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public double CountSalary()
+        {
+            double salarykoef;
+
+            switch (post.ToLower())
+            {
+                case "manager": salarykoef = 200;
+                    break;
+                case "developer": salarykoef = 150;
+                    break;
+                case "secretary": salarykoef = 80;
+                    break;
+                case "cleaner": salarykoef = 65;
+                    break;
+                default: salarykoef = 100;
+                    break;
+            }
+
+            switch (experience)
+            {
+                case 0: salarykoef *= 1.5;
+                    break;
+                case 1: salarykoef *= 2;
+                    break;
+                case 2: salarykoef *= 2.5;
+                    break;
+                default: salarykoef *= 5;
+                    break;
+            }
+
+            return salarykoef;
+        }
+
+        public void ShowSalary()
+        {
+            Console.WriteLine("Зарплата {0}, подоходный налог {1}", CountSalary(), CountSalary() * 0.13);
+        }
+    }
+
+
+}
---
+++ Salery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("John", "Smith");
+
+            employee.Post = "manager";
+            employee.Experience = 1;
+
+            Console.WriteLine(employee.Name +" " + employee.Surname +" " + employee.Post.ToUpper());
+            employee.ShowSalary();
+
+            Console.ReadKey();
+        }
+    }
+}
